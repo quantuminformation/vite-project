12b09e7e987814b22dc293765bdc4c57f698ffbc
Nikos Katsikanis <nkatsikanis@gmail.com>
2022-08-05 16:09:33 +0300

    Initial commit

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { describe, expect, it, vi, spyOn } from "vitest"
+
+
+
+it("true is true",  () => {
+ debugger
+
+  expect(true).toBeTruthy()
+})
